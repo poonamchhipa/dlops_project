2568ad76e7e4205c56f7e833f2e7a1613dad729b
poonamchhipa <poonamchhipa4@gmail.com>
2025-08-23 23:31:18 +0530

    evaluation_completed

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -44,3 +44,10 @@ class TrainingConfig:
     params_epochs: int 
 
 
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    params_image_size: list
+    params_batch_size: int
